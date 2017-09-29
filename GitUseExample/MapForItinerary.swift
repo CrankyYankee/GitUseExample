da41f58ef307e389be6d137bab41f2c5debe175e
Cranky Yankee <reasonbear@gmail.com>
2017-09-29 11:19:18 -0600

    Add new map-for-itinerary feature

---
+++ GitUseExample/MapForItinerary.swift
@@ -0,0 +1,13 @@
+//
+//  MapForItinerary.swift
+//  GitUseExample
+//
+//  Created by Randy Hudson on 9/29/17.
+//  Copyright © 2017 Randy Hudson. All rights reserved.
+//
+
+import UIKit
+
+class MapForItinerary: NSObject {
+   
+}
